mightysai1997/angular

feat(core): Add rxjs operator prevent app stability until an event
This commit adds an operator to help rxjs observables important for rendering
keep the application unstable (and prevent serialization) until there is
an event (observable emits, completes, or errors, or the subscription is
unsubscribed). This helps with SSR for zoneless and also helps for when
operations are intentionally executed outside the Angular Zone but are
still important for SSR (i.e. angularfire and the zoneWrap helper hacks).

packages/core/rxjs-interop/src/pending_until_event.ts
@@ -1,0 +1,69 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {
+  assertInInjectionContext,
+  ɵPendingTasks as PendingTasks,
+  inject,
+  Injector,
+} from '@angular/core';
+import {MonoTypeOperatorFunction, Observable} from 'rxjs';
+
+/**
+ * Operator which makes the application unstable until the observable emits, complets, errors, or is unsubscribed.
+ *
+ * Use this operator in observables whose subscriptions are important for rendering and should be included in SSR serialization.
+ *
+ * @param injector The `Injector` to use during creation. If this is not provided, the current injection context will be used instead (via `inject`).
+ *
+ * @experimental
+ */
+export function pendingUntilEvent<T>(injector?: Injector): MonoTypeOperatorFunction<T> {
+  if (injector === undefined) {
+    assertInInjectionContext(pendingUntilEvent);
+    injector = inject(Injector);
+  }
+  const taskService = injector.get(PendingTasks);
+
+  return (sourceObservable) => {
+    return new Observable<T>((originalSubscriber) => {
+      // create a new task on subscription
+      const task = taskService.add();
+
+      let cleanedUp = false;
+      function cleanupTask() {
+        if (cleanedUp) {
+          return;
+        }
+
+        taskService.remove(task);
+        cleanedUp = true;
+      }
+
+      const innerSubscription = sourceObservable.subscribe({
+        next: (v) => {
+          originalSubscriber.next(v);
+          cleanupTask();
+        },
+        complete: () => {
+          originalSubscriber.complete();
+          cleanupTask();
+        },
+        error: (e) => {
+          originalSubscriber.error(e);
+          cleanupTask();
+        },
+      });
+      innerSubscription.add(() => {
+        originalSubscriber.unsubscribe();
+        cleanupTask();
+      });
+      return innerSubscription;
+    });
+  };
+}

G-uday-kumar/be-early-be-safe-from-cancer

packages/core/rxjs-interop/src/pending_until_event.ts
@@ -1,0 +1,69 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+
+import {
+  assertInInjectionContext,
+  ExperimentalPendingTasks as PendingTasks,
+  inject,
+  Injector,
+} from '@angular/core';
+import {MonoTypeOperatorFunction, Observable} from 'rxjs';
+
+/**
+ * Operator which makes the application unstable until the observable emits, complets, errors, or is unsubscribed.
+ *
+ * Use this operator in observables whose subscriptions are important for rendering and should be included in SSR serialization.
+ *
+ * @param injector The `Injector` to use during creation. If this is not provided, the current injection context will be used instead (via `inject`).
+ *
+ * @experimental
+ */
+export function pendingUntilEvent<T>(injector?: Injector): MonoTypeOperatorFunction<T> {
+  if (injector === undefined) {
+    assertInInjectionContext(pendingUntilEvent);
+    injector = inject(Injector);
+  }
+  const taskService = injector.get(PendingTasks);
+
+  return (sourceObservable) => {
+    return new Observable<T>((originalSubscriber) => {
+      // create a new task on subscription
+      const removeTask = taskService.add();
+
+      let cleanedUp = false;
+      function cleanupTask() {
+        if (cleanedUp) {
+          return;
+        }
+
+        removeTask();
+        cleanedUp = true;
+      }
+
+      const innerSubscription = sourceObservable.subscribe({
+        next: (v) => {
+          originalSubscriber.next(v);
+          cleanupTask();
+        },
+        complete: () => {
+          originalSubscriber.complete();
+          cleanupTask();
+        },
+        error: (e) => {
+          originalSubscriber.error(e);
+          cleanupTask();
+        },
+      });
+      innerSubscription.add(() => {
+        originalSubscriber.unsubscribe();
+        cleanupTask();
+      });
+      return innerSubscription;
+    });
+  };
+}