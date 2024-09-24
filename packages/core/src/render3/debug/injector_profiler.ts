saqibumar/test-angular

feat(core): implement getSignalGraph debug API
Creates a debug api that returns two arrays of nodes and edges that represent a signal graph in the context of a particular injector.

packages/core/src/render3/debug/injector_profiler.ts
@@ -35,6 +35,11 @@ export const enum InjectorProfilerEventType {
    * Emits when an injector configures a provider.
    */
   ProviderConfigured,
+
+  /**
+   * Emits when an effect is created.
+   */
+  EffectCreated,
 }
 
 /**
@@ -74,6 +79,12 @@ export interface ProviderConfiguredEvent {
   providerRecord: ProviderRecord;
 }
 
+export interface EffectCreatedEvent {
+  type: InjectorProfilerEventType.EffectCreated;
+  context: InjectorProfilerContext;
+  effect: unknown;
+}
+
 /**
  * An object representing an event that is emitted through the injector profiler
  */
@@ -81,7 +92,8 @@ export interface ProviderConfiguredEvent {
 export type InjectorProfilerEvent =
   | InjectedServiceEvent
   | InjectorCreatedInstanceEvent
-  | ProviderConfiguredEvent;
+  | ProviderConfiguredEvent
+  | EffectCreatedEvent;
 
 /**
  * An object that contains information about a provider that has been configured
@@ -269,6 +281,16 @@ export function emitInjectEvent(token: Type<unknown>, value: unknown, flags: Inj
     type: InjectorProfilerEventType.Inject,
     context: getInjectorProfilerContext(),
     service: {token, value, flags},
+  });
+}
+
+export function emitEffectCreatedEvent(effect: unknown): void {
+  !ngDevMode && throwError('Injector profiler should never be called in production mode');
+
+  injectorProfiler({
+    type: InjectorProfilerEventType.EffectCreated,
+    context: getInjectorProfilerContext(),
+    effect,
   });
 }
 

G-uday-kumar/be-early-be-safe-from-cancer

packages/core/src/render3/debug/injector_profiler.ts
@@ -16,6 +16,7 @@ import {Type} from '../../interface/type';
 import {throwError} from '../../util/assert';
 import type {TNode} from '../interfaces/node';
 import type {LView} from '../interfaces/view';
+import {EffectRef} from '../reactivity/effect';
 
 /**
  * An enum describing the types of events that can be emitted from the injector profiler
@@ -35,6 +36,11 @@ export const enum InjectorProfilerEventType {
    * Emits when an injector configures a provider.
    */
   ProviderConfigured,
+
+  /**
+   * Emits when an effect is created.
+   */
+  EffectCreated,
 }
 
 /**
@@ -74,6 +80,12 @@ export interface ProviderConfiguredEvent {
   providerRecord: ProviderRecord;
 }
 
+export interface EffectCreatedEvent {
+  type: InjectorProfilerEventType.EffectCreated;
+  context: InjectorProfilerContext;
+  effect: EffectRef;
+}
+
 /**
  * An object representing an event that is emitted through the injector profiler
  */
@@ -81,7 +93,8 @@ export interface ProviderConfiguredEvent {
 export type InjectorProfilerEvent =
   | InjectedServiceEvent
   | InjectorCreatedInstanceEvent
-  | ProviderConfiguredEvent;
+  | ProviderConfiguredEvent
+  | EffectCreatedEvent;
 
 /**
  * An object that contains information about a provider that has been configured
@@ -269,6 +282,16 @@ export function emitInjectEvent(token: Type<unknown>, value: unknown, flags: Inj
     type: InjectorProfilerEventType.Inject,
     context: getInjectorProfilerContext(),
     service: {token, value, flags},
+  });
+}
+
+export function emitEffectCreatedEvent(effect: EffectRef): void {
+  !ngDevMode && throwError('Injector profiler should never be called in production mode');
+
+  injectorProfiler({
+    type: InjectorProfilerEventType.EffectCreated,
+    context: getInjectorProfilerContext(),
+    effect,
   });
 }
 